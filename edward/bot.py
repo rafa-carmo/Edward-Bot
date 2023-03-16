c9e1be4c53b5ef90451f7cea2827410c3dd539f7
Rafael do Carmo <rafaelcarmo143@gmail.com>
2023-03-15 23:26:27 -0300

    bot file from start

---
+++ edward/bot.py
@@ -0,0 +1,60 @@
+import discord
+from discord.ext import commands
+
+from edward.config import BASE_DIR, Ini
+
+COGS_DIR = BASE_DIR / "cogs"
+
+
+class Bot(commands.Bot):
+    """
+    Classe para iniciar o bot.
+
+    Extende de [discord.ext.commands.Bot](https://discordpy.readthedocs.io/en/stable/ext/commands/index.html)
+    Args:
+        servers: Lista opcional de servidores, vem diretamente do arquivo config.ini, deixando em branco ira aplicar a todos os servidores em que este bot for adicionado
+
+    """
+
+    def __init__(self, servers: list[int] = []):
+        intents = discord.Intents.default()
+        intents.message_content = True
+        self.sync = False
+        self.servers = (
+            [discord.Object(id=server) for server in servers]
+            if len(servers) > 0
+            else None
+        )
+        super().__init__(command_prefix="/", intents=intents)
+
+    async def setup_hook(self):
+        """
+        Função setup para forçar a sincronização de todos os commandos.
+        """
+        if not self.sync:
+            for command in COGS_DIR.glob("*.py"):
+                if command.name != "__init__.py":
+                    await self.load_extension(f"edward.cogs.{command.name[:-3]}")
+                    
+            await self.tree.sync(guild=self.servers)
+
+        print("BOT Started")
+
+    async def on_message(self, message: discord.Message):
+        """
+            Função para interceptar todas as mensagens que o bot tem acesso para aplicar os comandos.
+        """
+        if message.author == self.user:
+            return
+        if message.author.id == 966359640029933588:
+            ctx = await self.get_context(message)
+            if ctx.valid:
+                await self.invoke(ctx)
+
+        await self.process_commands(message)
+
+
+def start():
+    bot = Bot()
+    config = Ini()
+    bot.run(config.token, log_handler=None)

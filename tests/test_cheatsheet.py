01193ca5448407832a8a1b25e2a9d16014da3f8c
Rafael do Carmo <rafaelcarmo143@gmail.com>
2023-04-01 23:03:44 +0000

    mock test

--- tests/test_cheatsheet.py
+++ tests/test_cheatsheet.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 import meilisearch
 from discord import Embed
 from pytest import raises
@@ -19,10 +21,20 @@ def test_receive_raise_if_subject_not_in_list():
     assert error.value.args[0] == message_error
 
 
-def test_receive_raise_if_return_is_empty():
+@mock.patch("edward.cogs.cheatsheet.get_hits")
+def test_receive_raise_if_return_is_empty(mock_get_hits):
+    mock_get_hits.return_value = {
+        "hits": [],
+        "query": "branch",
+        "processingTimeMs": 0,
+        "limit": 20,
+        "offset": 0,
+        "estimatedTotalHits": 3,
+    }
+
     message_error = "Not found any relation"
-    client = meilisearch.Client("http://localhost:7700")
-    subjects = [index.uid for index in client.get_indexes()["results"]]
+    client = ""
+    subjects = ["git", "docker"]
 
     with raises(SearchNotFound) as error:
         search(
@@ -35,14 +47,26 @@ def test_receive_raise_if_return_is_empty():
     assert error.value.args[0] == message_error
 
 
-def test_have_to_return_a_discord_embed():
+@mock.patch("edward.cogs.cheatsheet.get_hits")
+def test_have_to_return_a_discord_embed(mock_get_hits):
+    mock_get_hits.return_value = {
+        "hits": [
+            {
+                "title": "Branches",
+                "content": "Criar branch e trocar para ela ```",
+                "codes": "",
+                "file_name": "index",
+                "id": "187104cd-5e02-4037-b534-ddf3f5176166",
+            }
+        ],
+    }
     message_error = "Not found any relation"
-    client = meilisearch.Client("http://localhost:7700")
-    subjects = [index.uid for index in client.get_indexes()["results"]]
+    client = ""
+    subjects = ["git", "docker"]
 
     embed = search(
-        subject="docker",
-        search="install",
+        subject="git",
+        search="branches",
         subjects=subjects,
         client=client,
         doc_url="http://teste.com",

5430dc0d9d6e2c5c2347557d84b8a996779d98be
Rafael do Carmo <rafaelcarmo143@gmail.com>
2023-04-01 22:32:02 +0000

    add doc_url from search

--- tests/test_cheatsheet.py
+++ tests/test_cheatsheet.py
@@ -9,7 +9,13 @@ def test_receive_raise_if_subject_not_in_list():
     message_error = "Subject not found"
 
     with raises(SubjectNotFound) as error:
-        search(subject="teste", search="teste", subjects=["outro"], client="")
+        search(
+            subject="teste",
+            search="teste",
+            subjects=["outro"],
+            client="",
+            doc_url="http://teste.com",
+        )
     assert error.value.args[0] == message_error
 
 
@@ -19,7 +25,13 @@ def test_receive_raise_if_return_is_empty():
     subjects = [index.uid for index in client.get_indexes()["results"]]
 
     with raises(SearchNotFound) as error:
-        search(subject="git", search="teste", subjects=subjects, client=client)
+        search(
+            subject="git",
+            search="teste",
+            subjects=subjects,
+            client=client,
+            doc_url="http://teste.com",
+        )
     assert error.value.args[0] == message_error
 
 
@@ -28,6 +40,12 @@ def test_have_to_return_a_discord_embed():
     client = meilisearch.Client("http://localhost:7700")
     subjects = [index.uid for index in client.get_indexes()["results"]]
 
-    embed = search(subject="docker", search="install", subjects=subjects, client=client)
+    embed = search(
+        subject="docker",
+        search="install",
+        subjects=subjects,
+        client=client,
+        doc_url="http://teste.com",
+    )
 
     assert isinstance(embed, Embed)
